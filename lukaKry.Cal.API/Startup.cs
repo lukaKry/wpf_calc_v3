5491e5d8622e1a7bcedf57e93abd602d5a07b3a3
lukaKry <lukasz.kryszak@codeconcept.pl>
2021-09-14 11:25:35 +0200

    docker enabled for api project

--- lukaKry.Cal.API/Startup.cs
+++ lukaKry.Cal.API/Startup.cs
@@ -32,7 +32,11 @@ namespace lukaKry.Calc.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CalculationDataContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            // local db
+            //services.AddDbContext<CalculationDataContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            // dockerized db
+            services.AddDbContext<CalculationDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AppDbContext")));
 
             services.AddSingleton<CalculationsFactoryProvider>();
             services.AddScoped<CalculatorService>();
